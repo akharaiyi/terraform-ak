b6c30e0cdc6c3aac4a9c56a65a29c1a7e43d2d0b
ak <ak@aks-MacBook-Pro.local>
2019-12-14 20:04:34 -0600

    second push for network

--- resource.tf
+++ resource.tf
@@ -4,4 +4,46 @@ resource "aws_vpc"  "environment-resource-expample"{
   enable_dns_support = true
   tags = { Name:"aws_terraform"}
 }
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-resource-expample.cidr_block, 3, 1 )}"
+  vpc_id = "${aws_vpc.environment-resource-expample.id}"
+  availability_zone = "us-west-1c"
 
+  tags = {Name:"private_subnet"}
+}
+
+resource "aws_subnet" "public_subnet" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-resource-expample.cidr_block,2 ,2 )}"
+  vpc_id = "${aws_vpc.environment-resource-expample.id}"
+  availability_zone = "us-west-1a"
+
+  tags = {Name:"public_subnet"}
+}
+
+resource "aws_security_group" "my_network_security" {
+  vpc_id = "${aws_vpc.environment-resource-expample.id}"
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-resource-expample.cidr_block}"
+      ]
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  }
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-resource-expample.cidr_block}"
+    ]
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+  }
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-resource-expample.cidr_block}"
+    ]
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+  }
+}
